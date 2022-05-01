f590170588c7efb88ede281781da94ea7007faf7
David Andersson <david.andersson@zocom.se>
2022-05-01 15:17:30 +0200

    Upgraded to Vue 3

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <h1>App colors</h1>
+        <ColorList />
+    </div>
+</template>
+
+<script>
+import ColorList from './components/ColorList.vue'
+
+export default {
+    name: 'App',
+    components: {
+        ColorList
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /* text-align: center; */
+    color: #2c3e50;
+    /* margin-top: 60px; */
+}
+</style>
